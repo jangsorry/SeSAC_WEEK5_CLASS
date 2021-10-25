19750b64519354e3f51c439635309f90f795d1c5
jangsorry <piccolox@naver.com>
2021-10-26 02:59:13 +0900

    Initial Commit

---
+++ SeSAC_WEEK5_CLASS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC_WEEK5_CLASS
+//
+//  Created by 지성 on 2021/10/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
